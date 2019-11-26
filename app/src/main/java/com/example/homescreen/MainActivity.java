e8b5efcd6f74ea71cff401dca34adcdd3cfedfd8
Saqib Arsalan Ijaz <saqibarsalan1997@gmail.com>
2019-11-26 10:53:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/homescreen/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.homescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sign_up();
+            }
+        });
+
+    }
+    public void sign_up() {
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
+    }
+
