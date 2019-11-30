71376dc1ec7f076acc0f358e10cd847454d910b6
Saqib Arsalan Ijaz <saqibarsalan1997@gmail.com>
2019-11-30 14:51:23 +0500

    Recycler view adapter.

--- app/src/main/java/com/example/homescreen/meeting_calender.java
+++ app/src/main/java/com/example/homescreen/meeting_calender.java
@@ -1,11 +1,14 @@
 package com.example.homescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.GridView;
@@ -13,51 +16,57 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class meeting_calender extends AppCompatActivity {
+
+    private static final String TAG = "JobsFeed";
+
+    private ArrayList<String> nNmaes = new ArrayList<>();
+    private ArrayList<Integer> nImageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting_calender);
-    }
+        initImageBitmaps();
 
-    // calendar components
-    LinearLayout header;
-    Button btnToday;
-    ImageView btnPrev;
-    ImageView btnNext;
-    TextView txtDateDay;
-    TextView txtDisplayDate;
-    TextView txtDateYear;
-    GridView gridView;
+    }
+    
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+
+
+        nImageUrls.add(R.drawable.cheetos);
+        nNmaes.add("Cheetah");
+
+        nImageUrls.add(R.drawable.person_1);
+        nNmaes.add("First Person");
+
+
+        nImageUrls.add(R.drawable.person_2);
+        nNmaes.add("Second Person");
+
+        initRecyclerView();
+
+    }
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: init_recyclerView");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,nNmaes,nImageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
 
     public meeting_calender() {
 
     }
 
-    public meeting_calender(Context context, AttributeSet attrs) {
-        initControl(context, attrs);
-    }
 
-    @SuppressLint("WrongViewCast")
-    private void assignUiElements() {
-        // layout is inflated, assign local variables to components
-        header = findViewById(R.id.calendar_header);
-        btnPrev = findViewById(R.id.calendar_prev_button);
-        btnNext = findViewById(R.id.calendar_next_button);
-        txtDateDay = findViewById(R.id.date_display_day);
-        txtDateYear = findViewById(R.id.date_display_year);
-        txtDisplayDate = findViewById(R.id.date_display_date);
-    }
 
     /**
      * Load control xml layout
      */
-    private void initControl(Context context, AttributeSet attrs)
-    {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        //inflater.inflate(R.layout.custom_calendar, this);
-        assignUiElements();
-    }
 
 }
